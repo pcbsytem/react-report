190098f2d56351cfbfedaccfda7af7bf7bfbb4bc
patrick.barcelos <patrick.barcelos@orgsystem.com.br>
2020-08-12 10:05:36 -0300

    Include structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+export default class App extends Component {
+  componentDidMount() {
+    this.addStylet("../stimulsoft/stimulsoft.viewer.office2013.whiteblue.css");
+    this.addScript("../stimulsoft/stimulsoft.reports");
+    this.addScript("../stimulsoft/stimulsoft.viewer");
+
+    console.log(window.Stimulsoft);
+  }
+
+  addScript(path) {
+    const script = document.createElement("script");
+    script.src = path;
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+  addStylet(path) {
+    const style = document.createElement("link");
+    style.rel = "stylesheet";
+    style.href = path;
+    document.body.appendChild(style);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>teste</h1>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://orgrelatorios.azurewebsites.net/Viewer"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Teste
         </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+        <iframe
+          height="100%"
+          width="100%"
+          src="https://orgrelatorios.azurewebsites.net/Viewer"
+          frameborder="0"
+          style={{ minHeight: "100vw" }}
+        ></iframe>
+      </>
+    );
+  }
+}
